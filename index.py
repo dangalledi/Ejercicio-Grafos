1c09fcbef3574cbafb5af7148062b9f154fb9fe9
Daniela Galleguillos <dangalledi@gmail.com>
2020-04-15 02:29:25 -0400

    CHG: Se arreglan rutas para usar forms

--- index.py
+++ index.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, request, url_for, redirect
+from forms import IngresoGrafo
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/tarea1', strict_slashes=False)
+@app.route('/tarea1', methods=['GET', 'POST'])
 def tarea1():
-    return render_template("tarea1.html")
+    form = IngresoGrafo()
+    return render_template("tarea1.html",title="ingrese grafo", form=form)
 
 if __name__=='__main__':
     app.run(debug=True)
