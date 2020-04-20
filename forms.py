8c4913b916c2736e00c5182568005a131af4971b
Daniela <dangalledi@gmail.com>
2020-04-20 11:47:53 -0400

    FIX: Validadores del formulario

--- forms.py
+++ forms.py
@@ -1,9 +1,9 @@
 from wtforms import Form
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, validators
+
 
 #Creacion de clases donde se reciben los datos del formulario 
 class IngresoGrafo(Form): 
-    vertices= StringField('Vertices',validators=[DataRequired()])
-    aristas= StringField('Aristas',validators=[DataRequired()])
+    vertices= StringField('Vertices',[validators.DataRequired(),validators.length(min=1,max=3, message='Ingrese la cantidad de vertices')])
+    aristas= StringField('Aristas',[validators.DataRequired(),validators.length(min=1,max=3, message='Ingrese la cantidad de aristas')])
     submit = SubmitField('Ingresar')
