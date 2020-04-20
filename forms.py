ffd44125165e468c251e9b539cc570c04954027e
Edgar Matus <63742939+garimatus@users.noreply.github.com>
2020-04-20 11:45:37 -0400

    tipo grafo

--- forms.py
+++ forms.py
@@ -1,9 +1,10 @@
 from wtforms import Form
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 #Creacion de clases donde se reciben los datos del formulario 
 class IngresoGrafo(Form): 
-    vertices= StringField('Vertices',validators=[DataRequired()])
-    aristas= StringField('Aristas',validators=[DataRequired()])
+    #ipo = SelectField(u'tipo',choices = [('Completo','completo'),('Direccionado','direccionado')])
+    vertices = StringField('Vertices',validators=[DataRequired()])
+    aristas = StringField('Aristas',validators=[DataRequired()])
     submit = SubmitField('Ingresar')
