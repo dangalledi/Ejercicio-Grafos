5cdf6351e931d9db0e31886a57a213fa844bfe85
Edgar <matus.edgar@gmail.com>
2020-04-28 17:40:03 -0400

    ADD: se agrega el algoritmo para ingresar el nombre de cada nodo al grafo

--- index.py
+++ index.py
@@ -29,20 +29,34 @@ def tarea1():
         #print ("eitquetado:",grafo.etiquetado.data)
         print ("tipo de grafo:",grafo.tipo.data)
         if  (grafo.nodos.data).isdigit() == True: #grafo.etiquetado.data == False and# de ser dígitos lo toma como la cantidad de nodos
+            grafo.etiquetado.data = False
             setattr(grafo,'vertices',int(grafo.nodos.data))  
-            return render_template("grafo.html", nodes = getattr(grafo,'vertices'), tipo = grafo.tipo.data, nombre = grafo.nombre.data)
-        else:
-            nodos = grafo.nodos.data.split(',') # de no ser dígitos lo toma como etiquetas y cantidad de nodos
-            print ("nodos etiquetados:",nodos)
-            setattr(grafo,'vertices',len(nodos))
             print ("cantidad de vertices del grafo:",getattr(grafo,'vertices'))
-            return render_template("grafo.html", nodes = getattr(grafo,'vertices'), tipo = grafo.tipo.data, nombre = grafo.nombre.data)
+            return render_template("grafo.html", nodes = getattr(grafo,'vertices'), type = grafo.tipo.data, name = grafo.nombre.data, labels = grafo.nodos.data, labeled = grafo.etiquetado.data)
+        else:
+            etiquetas = grafo.nodos.data.split(',') # de no ser dígitos lo toma como etiquetas y cantidad de nodos
+            grafo.etiquetado.data = True
+            print ("nodos etiquetados:",etiquetas)
+            setattr(grafo,'vertices',len(etiquetas))
+            print ("cantidad de vertices del grafo:",getattr(grafo,'vertices'))
+            return render_template("grafo.html", nodes = getattr(grafo,'vertices'), type = grafo.tipo.data, name = grafo.nombre.data, labels = grafo.nodos.data, labeled = grafo.etiquetado.data)
     return render_template("tarea1.html", grafo = grafo)
 
-@app.route('/graph/<int:nodes><string:nombre><string:tipo>')
-def graph(nodes,tipo,nombre):
-    G = nx.complete_graph(nodes)
-    nx.draw(G)
+@app.route('/graph/<int:nodes>/<string:type>/<string:name>/<string:labels>/<int:labeled>')
+def graph(nodes,type,name,labels,labeled):
+    #G = nx.complete_graph(nodes)
+    G = nx.Graph()
+    etiquetas = labels.split(',')
+    dic_etiquetas = { i : etiquetas[i] for i in range(len(etiquetas))}   
+    print(dic_etiquetas.items())
+    for i in range(nodes):
+        G.add_node(i)
+        print(G.nodes())
+    pos = nx.spring_layout(G)
+    if labeled == False:
+        nx.draw(G,with_labels=True)
+    else:
+        nx.draw_networkx_labels(G,pos,dic_etiquetas,font_size=11)
     img = BytesIO() # file-like object for the image
     plt.savefig(img) # save the image to the stream
     img.seek(0) # writing moved the cursor to the end of the file, reset
