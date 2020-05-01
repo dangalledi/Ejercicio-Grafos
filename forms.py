482fa1b751ed3eaf5dd2848e6cfe940dadbd761d
Dani <dangalledi@gmail.com>
2020-04-30 23:33:21 -0400

    DEL : Se elimino el nombre

--- forms.py
+++ forms.py
@@ -1,11 +1,14 @@
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField,Form
+from wtforms import StringField, SubmitField, SelectField,Form
 from wtforms.validators import DataRequired
-import networkx as nx  
 
 #Creacion de clases donde se reciben los datos del formulario 
 class ClaseGrafo(Form): 
-    nombre = StringField("Nombre",validators=[DataRequired()])
     tipo = SelectField(u'tipo',choices = [('simple','Simple'),('direccionado','Direccionado')])
     vertices = StringField('Vertices',validators=[DataRequired()])
     aristas = StringField('Aristas',validators=[DataRequired()])
+    submit = SubmitField('Ingresar')
+
+class IngresoPeso(Form):
+    arista= StringField('Arista',validators=[DataRequired()])
+    peso= StringField('Peso',validators=[DataRequired()])
     submit = SubmitField('Ingresar')
