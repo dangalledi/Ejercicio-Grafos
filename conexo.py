a19edf68be4b05d37d91476be8d7315fa0affde9
Jennifer <jennifer.portinof@utem.cl>
2020-05-03 02:12:27 -0400

    Arreglando funcion conexo

--- conexo.py
+++ conexo.py
@@ -15,3 +15,29 @@ def Matriz(grafo,n):
         aux[a-1][b-1]=1
         aux[b-1][a-1]=1
     return aux
+
+def BFS(s,M,n):
+    visitado=np.zeros(n) #arreglo de ceros, cambia a 1 si es visitado
+    cola = Queue()
+    cola.put(s)
+    visitado[s]=1
+    while not cola.empty():
+        s = cola.get()
+        for i in range(nodos):
+            if (M[s][i]==1):
+                cola.put(i)
+                visitado[i] = 1
+    return visitado
+
+matriz=Matriz(G2,nodos)
+print(matriz)
+visit = BFS(0,matriz,nodos)
+print(visit)
+
+#Matriz
+#    0 1 2 3 4
+#0   0 1 0 1 0   1
+#1   1 0 1 0 1   2
+#2   0 1 0 0 0   3
+#3   1 0 0 0 1   4 
+#4   0 1 0 1 0   5
